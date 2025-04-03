330b569cfbb4b50ff168ab2028dd200ba4654352
ElmahdyDev <wevengg@gmail.com>
2025-04-03 08:49:27 +0200

    Update pomodoro.vue

--- src/pages/pomodoro.vue
+++ src/pages/pomodoro.vue
@@ -11,7 +11,7 @@
         @click="setMode('shortBreak')" 
         :class="{ active: currentMode === 'shortBreak' }"
       >
-        Short Break
+        Elblbl
       </button>
       <button 
         @click="setMode('longBreak')" 
