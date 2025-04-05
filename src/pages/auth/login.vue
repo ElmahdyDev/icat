83d7824027cd4be078f9092f0ebdb062efc28e94
ElmahdyDev <wevengg@gmail.com>
2025-04-05 10:23:23 +0200

    Update src\pages\auth\login.vue

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -60,7 +60,7 @@ async handleLogin() {
       this.isLoading = true;
       
       try {
-        const response = await fetch('http://localhost:5000/api/auth/login', {
+        const response = await fetch('https://serveriicat.vercel.app/api/auth/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
