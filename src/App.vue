c5913e81dfc9b38be69a1508c85ae1a14304757d
ElmahdyDev <wevengg@gmail.com>
2025-04-06 17:29:36 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,16 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view v-slot="{ Component, route }">
-      <transition 
-        :name="'page'" 
-        :class="transitionClass"
-        @before-leave="beforeLeave"
-        @enter="enter">
-        <component :is="Component" :key="route.path" />
-      </transition>
-    </router-view>
+    <router-view />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
 import Navbar from './components/navbar.vue'
 export default {
-  setup() {
-    const transitionClass = ref('');
-    const beforeLeave = (el) => {
-      el.classList.add(transitionClass.value);
-    };
-    
-    const enter = (el) => {
-      el.classList.add(transitionClass.value);
-    };
-    window.addEventListener('swipe-left', () => {
-      transitionClass.value = 'swipe-left';
-    });
-    
-    window.addEventListener('swipe-right', () => {
-      transitionClass.value = 'swipe-right';
-    });
-    
-    return {
-      transitionClass,
-      beforeLeave,
-      enter
-    };
-  }
   name: 'App',
   components: {
     Navbar
   }
 }
 </script>
-<style>
-@import './assets/transitions.css';
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import setupSwipeNavigation from './swipeNavigation'
 import './style.css'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-setupSwipeNavigation(router)
