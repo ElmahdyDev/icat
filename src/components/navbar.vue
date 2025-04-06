72619e847cf4f873f559210189a399422d626890
ElmahdyDev <wevengg@gmail.com>
2025-04-06 17:44:26 +0200

    Update

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -64,7 +64,7 @@ export default {
     },
     closeMenu() {
       this.isMenuOpen = false
-    }
+    },
     checkAuthStatus() {
       this.isLoggedIn = authService.isAuthenticated();
       if (this.isLoggedIn) {
